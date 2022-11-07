482d6653e38a24838eec2c29373a61ad5fdc2f2f
bprafulkumar <prafulkumar176@gmail.com>
2022-11-07 15:36:54 +0530

    started to build chat top ; created room description modal

---
+++ src/components/chat-window/top/RoomInfoBtnModal.js
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react'
+import { Button, Modal } from 'rsuite'
+import { CurrentRoomContext } from '../../../Context/current-room.context'
+import { useModelState } from '../../../misc/custom-hooks'
+
+function RoomInfoBtnModal() {
+    const details = useContext(CurrentRoomContext)
+
+    const {isOpen ,close, open } = useModelState();
+
+  return (
+    <>
+        <Button appearance='link' className='px-0' onClick={open}>
+            Room information
+        </Button>
+
+        <Modal show={isOpen} onHide={close}>
+
+            <Modal.Header >
+                About {details.name}
+            </Modal.Header>
+
+            <Modal.Body>
+                <h6 className='mb-1'>Description</h6>
+                <p>{details.description}</p>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button block onClick={close}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    </>
+  )
+}
+
+export default RoomInfoBtnModal
--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,14 +1,40 @@
 import React, { memo, useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { ButtonToolbar, Icon } from 'rsuite'
 import { CurrentRoomContext, CurrentRoomProvider, useCurrentRoom } from '../../../Context/current-room.context'
+import { useMediaQuery } from '../../../misc/custom-hooks'
+import RoomInfoBtnModal from './RoomInfoBtnModal'
+// import Sidebar from '../../sidebar'
 
 function Top() {
     // const name = useCurrentRoom()
     const details = useContext(CurrentRoomContext)
     // console.log("detaiks",details)
-    // console.log(Object.values(details), "objetcs")
+    const isMobile = useMediaQuery('(max-width :992px)')
   return (
     <div>
-       {details.name}
+       <div style={{display:'flex', justifyContent:'space-between', alignItems: "center"}}>
+        <div>
+        <h4>
+          {
+            isMobile &&
+            <Icon componentClass={Link} to ="/" icon={"arrow-circle-left"} size="2x" className={isMobile ? 'd-inline-block p-0 mr-2 text-blue link-unstyled' : 'd-none'} />
+          }
+          <span className='text-disappear'>{details.name}</span>
+        </h4>
+        </div>
+        <div>
+
+        <ButtonToolbar className='ws-nowrap'>
+          todo
+        </ButtonToolbar>
+        </div>
+       </div>
+       <div style={{display:'flex', justifyContent:'space-between', alignItems: "center"}}>
+        <span>todo</span>
+        <RoomInfoBtnModal/>
+       </div>
+
     </div>
   )
 }
