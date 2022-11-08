3764a2d17f6b0c3d154f0915a2fe117576f74212
bprafulkumar <prafulkumar176@gmail.com>
2022-11-08 13:24:17 +0530

    displayed messages in chat box

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router';
+import { database } from '../../../misc/firebase'
+import trasformToArrWithId from '../../../misc/helper';
+import MessageItem from './messagesItem';
 
 function Messages() {
+
+  const {id} = useParams();
+  const [messages, setMessages] = useState(null)
+
+  const isChatEmpty = messages && messages.length === 0;
+  const canShowMessages = messages && messages.length > 0;
+
+  useEffect(() => {
+    const messagesRef = database.ref('/messages');
+
+    messagesRef.orderByChild('roomId').equalTo(id).on('value',(snap)=>{
+      const data =trasformToArrWithId(snap.val());
+
+      setMessages(data)
+    })
+  
+    return () => {
+      messagesRef.off('value')
+    }
+  }, [id]);
+  
   return (
-    <div>
-Messages
-    </div>
+    <ul className='msg-list custome-scroll'>
+      {isChatEmpty && <li>No messages yet</li>}
+      {canShowMessages && messages.map(msg => <MessageItem key={msg.id} message = {msg}/>)}
+    </ul>
   )
 }
 
---
+++ src/components/chat-window/messages/messagesItem.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import TimeAgo from 'timeago-react'
+import Profileavatar from '../../Dashboard/Profileavatar'
+
+function MessageItem({message}) {
+  const {author,createdAt , text} = message
+  return (
+
+    <li className='padded mb-1'>
+      <div className=' d-flex align-items-center font-bolder mb-1'>
+        <Profileavatar src = {author.avatar} name={author.name} className='m1-1' size = 'xs' />
+
+        <span className='ml-2'>{author.name}</span>
+        <TimeAgo datetime={createdAt} className='font-normal text-black-45 ml-2'/>
+        </div>
+
+        <div>
+          <span className='word-break-all'>{text}</span>
+        </div>
+    </li>
+   
+  )
+}
+
+export default MessageItem
